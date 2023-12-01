73aca316a0ba2a37310a80ee629920b3851c707c
rad0r0man <radoeom19@gmail.com>
2023-12-02 00:02:02 +0100

    Solve: Implement day1A solution

---
+++ day1A/main.py
@@ -0,0 +1,27 @@
+from tools.puzzle_reader import puzzle_reader
+
+# test_puzzle = ["1abc2", "pqr3stu8vwx", "a1b2c3d4e5f", "treb7uchet"]
+
+
+def create_int(string: str) -> int:
+    left = None
+    right = None
+    for letter in string:
+        if left is None and letter.isdigit():
+            left = letter
+
+    for letter in reversed(string):
+        if right is None and letter.isdigit():
+            right = letter
+
+    return int(left + right)
+
+
+def main():
+    puzzle_input = puzzle_reader("day1A/puzzle_input.txt")
+
+    print(sum([create_int(string) for string in puzzle_input]))
+
+
+if __name__ == "__main__":
+    main()
